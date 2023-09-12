efe5a6bdca08141489e1fcc938b54eda240bfdef
Joel Antony <joel.geoanto@gmail.com>
2023-09-12 16:35:57 +0530

    Got the template

--- src/Condition.js
+++ src/Condition.js
@@ -1,7 +1,18 @@
 import React from 'react'
 import './Condition.css'
 
-const Condition = ({conditionCheck,isNext}) => {
+const data=[
+  {rule:"Rule 1",desc:"Your password must be at least 5 characters",curr:false,isNext:false,truth:false},
+  {rule:"Rule 2",desc:"Your password must contain a number",curr:false,isNext:false,truth:false},
+  {rule:"Rule 3",desc:"Your password must contain an uppercase letter",curr:false,isNext:false,truth:false},
+]
+//the above is a template
+
+//curr is a property which will display the component...only for the 1st Cbox curr will be activated based on some condition 
+//After the 1st Cbox the value of curr is depends on the isNext property
+//truth property changes the colour of the box to either red or green if it is wrong or correct resp.
+
+const Condition = ({rulename,ruledesc,ruleFunc}) => {
 
   return (
     <div className='rule'>
--- src/PasswordCheck.js
+++ src/PasswordCheck.js
@@ -7,15 +7,3 @@ function WordCount(s){
         return false;
     }
 }//rule 1
-
-function IsDigit(s){
-    //does the password have a digit? If yes,return true
-    //otherwise return false
-}//rule 2
-
-function HasUpperCase(s){
-    //does the password have at least 1 uppercase character if yes,return true
-    //otherwise return false
-}//rule 3
-
-//dont know if this will work like this?
