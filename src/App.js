f4ec0754facb301fd3da0788befda938f13616c1
Joel Antony <joel.geoanto@gmail.com>
2023-11-04 12:50:22 +0530

    Fixed a minor bug with 1st rule

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { useState,useEffect,useRef } from 'react';
 import Condition from './Conditions/Condition'
 // import { CSSTransition,TransitionGroup } from 'react-transition-group';
 
-import {AddUptoFive, CaptchaCheck, Check2letterElem, CountCheck,HasRomanNumeral,HasSponsors,LanguageBarrier,LeapYearCheck,MonthofYear,NumberCheck,PeriodicSum,SpecialCheck,UpperCheck} from './PasswordCheck'
+import * as pwdCheck from './PasswordCheck'
 import Sponsor from './Conditions/Sponser';
 import Captcha from './Conditions/Captcha';
 import Gameover from './Gameover';
@@ -14,9 +14,14 @@ function App() {
   const [password,setPassword]=useState('')
   const [wordCount,setWordCount]=useState(0)
   const [nextCount,setNextCount]=useState(0)
+  const [displayFirstRule,setDFR]=useState(false)
 
   const [isPaulDed,setisPaulDed]=useState(false)
   const [canPaulbekilled,setCanPaulbekilled]=useState(false)
+
+  const [fireIgnition,setFireIgnition]=useState(true)
+  //this state is used for the FireIn function where if it is true then it will automatically return
+  //true and not check anything
 
   const stateRef_paulded=useRef();
   const stateRef_canPauldie=useRef();
@@ -60,22 +65,45 @@ function App() {
 
   }
 
+  const fireIn=(pwd)=>{
+
+    if(pwd.includes('🔥') && !fireIgnition){
+      setFireIgnition(true)
+    }
+
+    if(fireIgnition){
+      let eggIndex=-1;
+      for(let i=0;i<pwd.length;i++){
+        if(pwd[i]==="🥚"){
+          eggIndex=i;
+          break;
+        }
+      }
+
+      if(eggIndex!=-1){
+
+      }
+    }
+
+
+  }
+
 
   const [data,setData]=useState([
-    {id:14,rule:"Rule 14",desc:"The password must contain the english Translation of the word in the box",execute:LanguageBarrier,curr:false,isNext:false,truth:false},
-    // {id:13,rule:"Rule 13",desc:"The elements in your password must have atomic numbers that add up to 200",execute:PeriodicSum,curr:false,isNext:false,truth:false},
-    // {id:12,rule:"Rule 12",desc:"This is my chicken Paul 🥚, he hasn't hatched yet, Please put him in your password and keep him safe",execute:HelloPaul,curr:false,isNext:false,truth:false},
-    // {id:11,rule:"Rule 11",desc:"Your password must include a leap year",execute:LeapYearCheck,curr:false,isNext:false,truth:false},
-    // {id:10,rule:"Rule 10",desc:"Your password must include a 2 letter symbol from the periodic table",execute:Check2letterElem,curr:false,isNext:false,truth:false},
-    // {id:9,rule:"Rule 9",desc:"Your password must include this CAPTCHA",execute:CaptchaCheck,curr:false,isNext:false,truth:false},
-    // {id:8,rule:"Rule 8",desc:"Your password must include our sponsors!",execute:HasSponsors,curr:false,isNext:false,truth:false},
-    // {id:7,rule:"Rule 7",desc:"Your password must contain a roman numeral",execute:HasRomanNumeral,curr:false,isNext:false,truth:false},
-    // {id:6,rule:"Rule 6",desc:"Your password must contain a month of the year",execute:MonthofYear,curr:false,isNext:false,truth:false},
-    // {id:5,rule:"Rule 5",desc:"The digits in your password must add upto 25",execute:AddUptoFive,curr:false,isNext:false,truth:false},
-    // {id:4,rule:"Rule 4",desc:"Your password must include a special character",execute:SpecialCheck,curr:false,isNext:false,truth:false},
-    // {id:3,rule:"Rule 3",desc:"Your password must contain an uppercase letter",execute:UpperCheck,curr:false,isNext:false,truth:false},
-    // {id:2,rule:"Rule 2",desc:"Your password must contain a number",execute:NumberCheck,curr:false,isNext:false,truth:false},
-    {id:1,rule:"Rule 1",desc:"Your password must be at least 5 characters",execute:CountCheck,curr:false,isNext:false,truth:false}
+    {id:14,rule:"Rule 14",desc:"The password must contain the english Translation of the word in the box",execute:pwdCheck.LanguageBarrier,curr:false,isNext:false,truth:false},
+    // {id:13,rule:"Rule 13",desc:"The elements in your password must have atomic numbers that add up to 200",execute:pwdCheck.PeriodicSum,curr:false,isNext:false,truth:false},
+    {id:12,rule:"Rule 12",desc:"This is my chicken Paul 🥚, he hasn't hatched yet, Please put him in your password and keep him safe",execute:HelloPaul,curr:false,isNext:false,truth:false},
+    // {id:11,rule:"Rule 11",desc:"Your password must include a leap year",execute:pwdCheck.LeapYearCheck,curr:false,isNext:false,truth:false},
+    // {id:10,rule:"Rule 10",desc:"Your password must include a 2 letter symbol from the periodic table",execute:pwdCheck.Check2letterElem,curr:false,isNext:false,truth:false},
+    // {id:9,rule:"Rule 9",desc:"Your password must include this CAPTCHA",execute:pwdCheck.CaptchaCheck,curr:false,isNext:false,truth:false},t
+    // {id:8,rule:"Rule 8",desc:"Your password must include our sponsors!",execute:pwdCheck.HasSponsors,curr:false,isNext:false,truth:false},
+    // {id:7,rule:"Rule 7",desc:"Your password must contain a roman numeral",execute:pwdCheck.HasRomanNumeral,curr:false,isNext:false,truth:false},
+    // {id:6,rule:"Rule 6",desc:"Your password must contain a month of the year",execute:pwdCheck.MonthofYear,curr:false,isNext:false,truth:false},
+    {id:5,rule:"Rule 5",desc:"The digits in your password must add upto 25",execute:pwdCheck.AddUptoFive,curr:false,isNext:false,truth:false},
+    {id:4,rule:"Rule 4",desc:"Your password must include a special character",execute:pwdCheck.SpecialCheck,curr:false,isNext:false,truth:false},
+    {id:3,rule:"Rule 3",desc:"Your password must contain an uppercase letter",execute:pwdCheck.UpperCheck,curr:false,isNext:false,truth:false},
+    {id:2,rule:"Rule 2",desc:"Your password must contain a number",execute:pwdCheck.NumberCheck,curr:false,isNext:false,truth:false},
+    {id:1,rule:"Rule 1",desc:"Your password must be at least 5 characters",execute:pwdCheck.CountCheck,curr:false,isNext:false,truth:false}
   ])
 
   const [display,setDisplay]=useState([])
@@ -88,8 +116,12 @@ function App() {
     newData=data.map((datax,index)=>{
       const newObj={...datax}
       if(datax.id===1){//only for the last element
-        if(wordCount>0 || nextCount>0){
-
+        if(wordCount>0 || displayFirstRule){
+          
+          if(!displayFirstRule){
+            setDFR(true)
+          }
+          
           if(!newObj.curr){
             newObj.curr=true;
           }
@@ -174,14 +206,6 @@ function App() {
     e.target.style.height=(e.target.scrollHeight)+"px"
   }
 
-  function BoldText(){
-    const selectedText=window.getSelection().toString()
-    let currPass=password
-    currPass=currPass.replace(selectedText,'<b>${selectedText}</b>')
-    setPassword(currPass)
-    console.log("This button is clicked")
-  }
-
   function PasswordLength(e){
     setPassword(e.target.value);
     WordSize(e)
@@ -241,7 +265,7 @@ function App() {
                   )
                 }
 
-                else if(datax.id==14){
+                else if(datax.id===14){
                   return(
                     <Translate key={datax.id} rulename={datax.rule} ruledesc={datax.desc} trueValue={datax.truth}/>
                   )
